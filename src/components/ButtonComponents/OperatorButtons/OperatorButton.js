06a0aabd35722c6445290ddecf87fdbaba8adfd8
Wanda <doubleuevans@gmail.com>
2019-08-24 14:49:40 -0500

    mapped and rendered Operator Buttons

--- src/components/ButtonComponents/OperatorButtons/OperatorButton.js
+++ src/components/ButtonComponents/OperatorButtons/OperatorButton.js
@@ -1,9 +1,13 @@
 import React from "react";
 
-export const OperatorButton = () => {
+const OperatorButton = props => {
+    console.log("Operator Button", props);
     return (
-        <>
+        <button>
             {/* Display a button element rendering the data being passed down from the parent container on props */}
-        </>
+            {props.operator.char}
+        </button>
     );
 };
+
+export default OperatorButton;
