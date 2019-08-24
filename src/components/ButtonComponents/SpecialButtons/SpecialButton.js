f329c3aeb32a9d4918c1f85fa02392711b143bd7
Wanda <doubleuevans@gmail.com>
2019-08-24 15:02:28 -0500

    rendering Sepcial Button

--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -1,9 +1,12 @@
 import React from "react";
 
-export const SpecialButton = () => {
+export const SpecialButton = props => {
     return (
-        <>
+        <button>
             {/* Display a button element rendering the data being passed down from the parent container on props */}
-        </>
+            {props.text}
+        </button>
     );
 };
+
+export default SpecialButton;
--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -1,19 +1,23 @@
-import React from "react";
-import SpecialButton from "../SpecialButtons/SpecialButton";
-import specials from "../../../data";
+import React, { useState } from "react";
 
 //import any components needed
+import SpecialButton from "./SpecialButton";
 
 //Import your array data to from the provided data file
+import { specials } from "../../../data";
 
 const Specials = () => {
     // STEP 2 - add the imported data to state
-
+    console.log("Specials", specials);
+    const [specButton] = useState(specials);
     return (
         <div>
             {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+            {specButton.map((special, i) => {
+                return <SpecialButton text={special} key={i} />;
+            })}
         </div>
     );
 };
