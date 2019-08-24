cc017f5f2a3782362db5441391dd371804ba1969
Wanda <doubleuevans@gmail.com>
2019-08-24 15:20:42 -0500

    removed console.log

--- src/components/ButtonComponents/OperatorButtons/OperatorButton.js
+++ src/components/ButtonComponents/OperatorButtons/OperatorButton.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const OperatorButton = props => {
-    console.log("Operator Button", props);
+    // console.log("Operator Button", props);
     return (
         <button onClick={() => console.log(props.operator.char)}>
             {/* Display a button element rendering the data being passed down from the parent container on props */}
