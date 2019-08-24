2d8ba760b3254919809c73e530de4745b0e333ae
Wanda <doubleuevans@gmail.com>
2019-08-24 15:12:08 -0500

    added onClicks to buttons

--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export const SpecialButton = props => {
     return (
-        <button>
+        <button onClick={() => console.log(props.text)}>
             {/* Display a button element rendering the data being passed down from the parent container on props */}
             {props.text}
         </button>
