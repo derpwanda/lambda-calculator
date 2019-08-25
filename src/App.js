21596878508de87e0d77e6f31395dc83bdd269c5
Wanda <doubleuevans@gmail.com>
2019-08-24 19:55:20 -0500

    displays number in Display

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Display from "./components/DisplayComponents/Display";
 import Numbers from "./components/ButtonComponents/NumberButtons/Numbers";
@@ -16,14 +16,18 @@ function App() {
     // Your functions should accept a parameter of the the item data being displayed to the DOM (ie - should recieve 5 if the user clicks on
     // the "5" button, or the operator if they click one of those buttons) and then call your setter function to update state.
     // Don't forget to pass the functions (and any additional data needed) to the components as props
+    const [calcValues, setCalcValues] = useState("0");
+    const addNumber = number => {
+        setCalcValues(calcValues => calcValues + number);
+    };
 
     return (
         <div className='container'>
             <Logo />
             <div className='App'>
                 {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
-                <Display number={6} />
-                <Numbers />
+                <Display number={calcValues} />
+                <Numbers addNumber={addNumber} />
                 <Operators />
                 <Specials />
             </div>
